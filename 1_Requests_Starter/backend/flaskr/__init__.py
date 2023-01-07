28fe629e7d8b27ffa39acd792a5116c3e21a7051
Laura <laura.kapitza@gmail.com>
2023-01-07 17:55:45 +0100

    create patch route to update a book - create delete route to delete a book by id

--- 1_Requests_Starter/backend/flaskr/__init__.py
+++ 1_Requests_Starter/backend/flaskr/__init__.py
@@ -8,12 +8,6 @@ import random
 
 BOOKS_PER_SHELF = 8
 
-# @TODO: General Instructions
-#   - As you're creating endpoints, define them and then search for 'TODO' within the frontend to update the endpoints there.
-#     If you do not update the endpoints, the lab will not work - of no fault of your API code!
-#   - Make sure for each route that you're thinking through when to abort and with which kind of error
-#   - If you change any of the response body keys, make sure you update the frontend to correspond.
-
 
 def create_app(test_config=None):
     # create and configure the app
@@ -32,12 +26,6 @@ def create_app(test_config=None):
         )
         return response
 
-    # @TODO: Write a route that retrieves all books, paginated.
-    #         You can use the constant above to paginate by eight books.
-    #         If you decide to change the number of books per page,
-    #         update the frontend to handle additional books in the styling and pagination
-    #         Response body keys: 'success', 'books' and 'total_books'
-    # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
     @app.get('/books')
     def get_books():
         page = request.args.get('page', 1, type=int)
@@ -52,22 +40,35 @@ def create_app(test_config=None):
             'total_books': len(formatted_books)
         })
 
-    # @TODO: Write a route that will update a single book's rating.
-    #         It should only be able to update the rating, not the entire representation
-    #         and should follow API design principles regarding method and route.
-    #         Response body keys: 'success'
-    # TEST: When completed, you will be able to click on stars to update a book's rating and it will persist after refresh
+    @app.patch('/books/<int:book_id>')
+    def update_rating(book_id):
+        book = Book.query.filter(Book.id == book_id).first_or_404()
+        updates = request.get_json()
+        book.rating = updates["rating"]
+        book.update()
 
-    # @TODO: Write a route that will delete a single book.
-    #        Response body keys: 'success', 'deleted'(id of deleted book), 'books' and 'total_books'
-    #        Response body keys: 'success', 'books' and 'total_books'
+        return jsonify({
+            'success': True
+        })
 
-    # TEST: When completed, you will be able to delete a single book by clicking on the trashcan.
+    @app.delete('/books/<int:book_id>')
+    def delete_book(book_id):
+        book = Book.query.filter(Book.id == book_id).one_or_none()
+        book.delete()
 
-    # @TODO: Write a route that create a new book.
-    #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
-    # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
-    #       Your new book should show up immediately after you submit it at the end of the page.
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * BOOKS_PER_SHELF
+        end = start + BOOKS_PER_SHELF
+
+        books = Book.query.all()
+        formatted_books = [book.format() for book in books]
+        return jsonify({
+            'success': True,
+            'deleted': book_id,
+            'books': formatted_books[start:end],
+            'total_books': len(formatted_books)
+        })
+
     @app.post('/books')
     def add_book():
         form = request.get_json()
